8ecc464e6ae24af03803470cbf518ddf59755782
jameskitonga <jameskitonga432@gmail.com>
2022-10-19 12:21:48 +0300

    finished installing bootstrap

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,10 +1,13 @@
-impo
+import RegisterItems from "../components/RegisterItems";
+import styled from "styled-components";
 
-
+const Container=styled.div``
 
 const Register = () => {
     return (
-        
+        <Container>
+            <RegisterItems />
+        </Container>
     )
 }
 export default Register;
--- src/components/RegisterItems.js
+++ src/components/RegisterItems.js
@@ -1,9 +1,42 @@
-import styled from "styled-components";
-import { useRef,useEffect,useState } from "react";
+
+import { useRef, useEffect, useState } from "react";
+import { faCheck, faTimes, faInfoCicrcle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+ 
+
+
+
+const userRegister = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const PwdRegister = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.[!@#$%]).{8,24}&/;
 
 const RegisterItems = () => {
+    const UserRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState("");
+    const [validName, setValidName] = useState(false);
+    const [UserFocus, setUserFocus] = useState(false);
+
+    const [Pwd, setPwd] = useState("");
+    const [ValidPwd, setValidPwd] = useState(false);
+    const [PwdFocus, SetPwdFocus] = useState(false);
+
+    const [matchPwd, setMatchPwd] = useState("");
+    const [validMatch, setValidMatch] = useState(false);
+    const [matchFocus,setMatchFocus]=useState(false)
+
+
+
+
     return (
-      
-  )  
+        <div>
+            
+    </div>
+
+
+
+    )
 }
 export default RegisterItems;
